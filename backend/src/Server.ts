8fb83b7d94a8aab4d962123804653557b00a4465
Girlie Razon <girlie.razon84@gmail.com>
2026-01-08 00:31:55 +0100

    fix(server): load env early, order middlewares correctly, start after DB connect

--- backend/src/Server.ts
+++ backend/src/Server.ts
@@ -1,23 +1,36 @@
-import express from 'express'
-import ApplyMiddlewares from './configurations/ApplyMiddlewares'
-import Configuration from './configurations/Configuration'
-import { notFound } from './middlewares/Middleware'
-import AliveRoutes from './routes/AliveRoutes'
-import UserRoutes from './routes/UserRoutes'
+import "dotenv/config";
+import express from "express";
+
+import ApplyMiddlewares from "./configurations/ApplyMiddlewares";
+import Configuration from "./configurations/Configuration";
+import { notFound, errorHandler } from "./middlewares/ErrorMiddleware";
+
+import AliveRoutes from "./routes/AliveRoutes";
+import UserRoutes from "./routes/UserRoutes";
 import PostRoutes from "./routes/PostRoutes";
 
-const app = express()
 
-ApplyMiddlewares(app)
+const app = express();
 
-AliveRoutes.routes(app)
-UserRoutes.routes(app)
-PostRoutes.routes(app)
+ApplyMiddlewares(app);
 
+AliveRoutes.routes(app);
+UserRoutes.routes(app);
+PostRoutes.routes(app);
 
-app.use(notFound)
+app.use(notFound);
+app.use(errorHandler);
 
-Configuration.connectToPort(app)
-Configuration.connectToDatabase().then()
+const start = async () => {
+  await Configuration.connectToDatabase();
+  Configuration.connectToPort(app);
+};
 
-export default app
+start().catch((err) => {
+  // last resort if startup fails
+  // eslint-disable-next-line no-console
+  console.error("Failed to start server", err);
+  process.exit(1);
+});
+
+export default app;
