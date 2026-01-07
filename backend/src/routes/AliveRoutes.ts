5037cf5ac45d4e763a0c21440bb2c1aa8aea3f8f
Girlie Razon <girlie.razon84@gmail.com>
2026-01-08 00:29:06 +0100

    chore(routes): type Alive route handler and simplify exports

--- backend/src/routes/AliveRoutes.ts
+++ backend/src/routes/AliveRoutes.ts
@@ -1,13 +1,11 @@
-import { Express } from 'express'
-import StatusCode from '../configurations/StatusCode'
+import type { Express, Request, Response } from "express";
+import StatusCode from "../configurations/StatusCode";
 
 
 const routes = (app: Express) => {
-    app.get('/', (req, res) => {
-        res.status(StatusCode.OK).send('My API is Alive!')
-    })
-}
+    app.get("/", (_req: Request, res: Response) => {
+        res.status(StatusCode.OK).send("My API is Alive!");
+    });
+};
 
-export default {
-    routes
-}
+export default { routes };
