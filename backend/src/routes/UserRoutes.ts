b11082fbdd64b88a544ea3bdae67718c549ba5e0
Girlie Razon <girlie.razon84@gmail.com>
2026-01-08 00:31:00 +0100

    chore(routes): normalize user route constants and typings

--- backend/src/routes/UserRoutes.ts
+++ backend/src/routes/UserRoutes.ts
@@ -1,21 +1,22 @@
-import { Express } from 'express'
-import UserController from '../controllers/UserController'
+import type { Express } from "express";
+import UserController from "../controllers/UserController";
 
-const usersUrl: string = '/users'
-const usersUrlWithId: string = `${usersUrl}/:userId`
-const searchUsers = `/searchUser`
-const verifyUserUrl = '/verifyUser'
+
+const usersUrl = "/users";
+const usersUrlWithId = `${usersUrl}/:userId`;
+const searchUsersUrl = "/searchUser";
+const verifyUserUrl = "/verifyUser";
 
 const routes = (app: Express) => {
-    app.post(usersUrl, UserController.createUser)
-    app.post(verifyUserUrl, UserController.verifyUser)
-    app.get(usersUrl, UserController.getAllUsers)
-    app.get(usersUrlWithId, UserController.getUserWithId)
-    app.get(searchUsers, UserController.getUserWithQuery)
-    app.put(usersUrlWithId, UserController.updateUser)
-    app.delete(usersUrlWithId, UserController.deleteUser)
-}
+    app.post(usersUrl, UserController.createUser);
+    app.post(verifyUserUrl, UserController.verifyUser);
 
-export default {
-    routes
-}
+    app.get(usersUrl, UserController.getAllUsers);
+    app.get(usersUrlWithId, UserController.getUserWithId);
+    app.get(searchUsersUrl, UserController.getUserWithQuery);
+
+    app.put(usersUrlWithId, UserController.updateUser);
+    app.delete(usersUrlWithId, UserController.deleteUser);
+};
+
+export default { routes };
