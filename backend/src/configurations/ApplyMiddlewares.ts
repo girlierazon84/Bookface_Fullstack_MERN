dcd9e3e8b39a443d2de0e3928fa7f63950d7a5f4
Girlie Razon <girlie.razon84@gmail.com>
2026-01-08 00:32:49 +0100

    fix(middleware): remove errorHandler from ApplyMiddlewares and keep base stack clean

--- backend/src/configurations/ApplyMiddlewares.ts
+++ backend/src/configurations/ApplyMiddlewares.ts
@@ -1,26 +1,23 @@
-import cors from 'cors'
-import express from 'express'
-import helmet from 'helmet'
-import { errorHandler } from '../middlewares/Middleware'
-import MorganMiddleware from '../middlewares/MorganMiddleware'
+import cors from "cors";
+import express from "express";
+import helmet from "helmet";
+import MorganMiddleware from "../middlewares/MorganMiddleware";
 
-// Middlewares
-const allowedOrigins = ['http://localhost:3000']
-// const allowedOrigins = ['*']
-const allowedMethods = ['GET', 'POST', 'PUT', 'DELETE']
+
+const allowedOrigins = ["http://localhost:3000"];
+const allowedMethods = ["GET", "POST", "PUT", "DELETE"];
 
 const options: cors.CorsOptions = {
     origin: allowedOrigins,
     methods: allowedMethods
-}
+};
 
 const ApplyMiddlewares = (app: express.Application) => {
-    app.use(helmet())
-    app.use(cors(options))
-    app.use(express.urlencoded({extended: false}))
-    app.use(express.json())
-    app.use(MorganMiddleware)
-    app.use(errorHandler)
-}
+    app.use(helmet());
+    app.use(cors(options));
+    app.use(express.urlencoded({ extended: false }));
+    app.use(express.json());
+    app.use(MorganMiddleware);
+};
 
-export default ApplyMiddlewares
+export default ApplyMiddlewares;
