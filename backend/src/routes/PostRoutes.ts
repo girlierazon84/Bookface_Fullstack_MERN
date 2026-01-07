f345c87e5cac9f436a9f0c99d0fcd775c6c3e8d2
Girlie Razon <girlie.razon84@gmail.com>
2026-01-08 00:30:01 +0100

    chore(routes): tighten post routes typing and constants

--- backend/src/routes/PostRoutes.ts
+++ backend/src/routes/PostRoutes.ts
@@ -1,17 +1,16 @@
-import { Express } from 'express'
-import PostController from '../controllers/PostController'
+import type { Express } from "express";
+import PostController from "../controllers/PostController";
 
-const postUrl: string = '/posts'
-const postUrlWithId: string = `${postUrl}/:postId`
+
+const postUrl = "/posts";
+const postUrlWithId = `${postUrl}/:postId`;
 
 const routes = (app: Express) => {
-    app.post(postUrl, PostController.createPost)
-    app.get(postUrl, PostController.getAllPosts)
-    app.get(postUrlWithId, PostController.getPostById)
-    app.put(postUrlWithId, PostController.updatePost)
-    app.delete(postUrlWithId, PostController.deletePost)
-}
+    app.post(postUrl, PostController.createPost);
+    app.get(postUrl, PostController.getAllPosts);
+    app.get(postUrlWithId, PostController.getPostById);
+    app.put(postUrlWithId, PostController.updatePost);
+    app.delete(postUrlWithId, PostController.deletePost);
+};
 
-export default {
-    routes
-}
+export default { routes };
