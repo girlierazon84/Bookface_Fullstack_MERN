a90012b06acc7ceda8d43a226a8be3236ec7c40c
Girlie Razon <girlie.razon84@gmail.com>
2026-01-08 19:16:38 +0100

    fix(auth): modernize login flow with async/await and route constants

--- frontend/src/view/UsersLogInView.tsx
+++ frontend/src/view/UsersLogInView.tsx
@@ -1,65 +1,69 @@
-import React, {useState} from 'react'
-import {Link, useNavigate} from 'react-router-dom'
-import styled from 'styled-components'
-import {PrimaryButton} from '../components/CustomButtonComponent'
-import RoutingPath from '../routes/RoutingPath'
-import UserService from '../utils/api/service/UserService'
-import {useUserContext} from '../utils/global/provider/UserProvider'
-import { UsersLogInDataObject } from '../utils/interface/UsersInterfaces'
+// frontend/src/view/UsersLogInView.tsx
+
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+import { PrimaryButton } from "../components/CustomButtonComponent";
+import RoutingPath from "../routes/RoutingPath";
+import UserService from "../utils/api/service/UserService";
+import { useUserContext } from "../utils/global/provider/UserProvider";
+import type { UsersLogInDataObject } from "../utils/interface/UsersInterfaces";
 
 
-export const UsersLogInView = () => {
-    const [userName, setUserName] = useState<string>('')
-    const [passWord, setPassWord] = useState<string>('')
-    const [loginText, setLoginText] = useState<string>('')
-    const {setAuthenticatedUser} = useUserContext()
+const UsersLogInView: React.FC = () => {
+  const [userName, setUserName] = useState("");
+  const [passWord, setPassWord] = useState("");
+  const [loginText, setLoginText] = useState("");
+  const { setAuthenticatedUser } = useUserContext();
+  const navigate = useNavigate();
 
-    const navigate = useNavigate()
+  const verifyUser = async () => {
+    const payload: UsersLogInDataObject = {
+      username: userName,
+      password: passWord
+    };
 
-    const verifyUser = () => {
-        const payload: UsersLogInDataObject = {
-            username: userName,
-            password: passWord,
-        }
-
-        UserService.verifyUser(payload)
-            .then(function (response) {
-                console.log(response.data.message)
-                login(response.data.message)
-            })
-            .catch(function (error) {
-                console.log(error)
-            })
+    try {
+      const res = await UserService.verifyUser(payload);
+      if (res.data.message) {
+        setAuthenticatedUser(userName);
+        localStorage.setItem("username", userName);
+        navigate(RoutingPath.profileView);
+      } else {
+        setLoginText("Wrong username or password");
+      }
+    } catch {
+      setLoginText("Login failed. Please try again.");
     }
+  };
 
-    const login = (apiResponse: boolean) => {
-        if (apiResponse) {
-            setAuthenticatedUser(userName)
-            localStorage.setItem('username', userName)
-            navigate(RoutingPath.profileView)
-        } else {
-            setLoginText('Wrong username or password')
-        }
-    }
+  return (
+    <Wrapper>
+      <h1>Bookface</h1>
 
-    return (
-        <Wrapper>
-            <h1>Bookface</h1>
-            <GridContainer>
-                <h4>Log in here...</h4>
-                <input type='text' placeholder='Username' onChange={event => setUserName(event.target.value)}/>
-                <input type='password' placeholder='Password' onChange={event => setPassWord(event.target.value)}/>
-            </GridContainer>
-            <br/>
-            <H3>{loginText}</H3>
-            <PrimaryButton onClick={() => verifyUser()} children={'Log In'}/>
-            <br/>
-            <H4>No account yet?  <Link to='/sign_up'>Sign Up</Link>  here!...</H4>
-        </Wrapper>
-    )
-}
+      <GridContainer>
+        <h4>Log in here...</h4>
+        <input type="text" placeholder="Username" onChange={(e) => setUserName(e.target.value)} />
+        <input
+          type="password"
+          placeholder="Password"
+          onChange={(e) => setPassWord(e.target.value)}
+        />
+      </GridContainer>
 
-export default UsersLogInView
+      <H3>{loginText}</H3>
+
+      <PrimaryButton onClick={verifyUser}>Log In</PrimaryButton>
+
+      <H4>
+        No account yet? <Link to={RoutingPath.signUpFormView}>Sign Up</Link> here!
+      </H4>
+    </Wrapper>
+  );
+};
+
+export default UsersLogInView;
 
 const Wrapper = styled.section`
   background-color: var(--primary-color);
@@ -69,10 +73,10 @@ const Wrapper = styled.section`
   h1 {
     font-weight: bold;
     font-size: 3.7em;
-    font-family: 'Oxygen - Regular', sans-serif;
+    font-family: "Oxygen - Regular", sans-serif;
     color: var(--secondary-color);
   }
-`
+`;
 
 const GridContainer = styled.div`
   display: grid;
@@ -81,8 +85,8 @@ const GridContainer = styled.div`
   box-shadow: 5px 10px 8px 5px var(--fourthly-color);
   border-radius: 20px;
   padding: 0 2em 3.5em 2em;
-  grid-gap: 2.5em;
-  
+  gap: 2.5em;
+
   h4 {
     color: var(--fourthly-color);
     font-family: "Lucida Calligraphy", sans-serif;
@@ -90,24 +94,27 @@ const GridContainer = styled.div`
     width: 100%;
     text-align: left;
   }
-  
+
   input {
     width: 100%;
-    height: 6.5vh;
+    height: 48px;
     border: none;
     box-shadow: 2px 4px 3px 2px var(--fourthly-color);
     border-radius: 10px;
     padding-left: 20px;
     font-size: 1em;
   }
-`
+`;
 
 const H3 = styled.h3`
   color: red;
   font-weight: bold;
-`
+  min-height: 24px;
+  margin: 16px 0;
+`;
 
 const H4 = styled.h4`
   color: var(--fourthly-color);
   font-weight: 700;
-`
+  margin-top: 16px;
+`;
