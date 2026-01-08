e6f61ea4fb53db5d75be0c219a7af16a8fc13a7f
Girlie Razon <girlie.razon84@gmail.com>
2026-01-08 15:10:29 +0100

    chore(types): normalize user interfaces and add UpdateUserObject

--- frontend/src/utils/interface/UsersInterfaces.ts
+++ frontend/src/utils/interface/UsersInterfaces.ts
@@ -1,23 +1,22 @@
+// frontend/src/utils/interface/UsersInterfaces.ts
+
 export interface CreateUserObject {
-    firstname: string
-    lastname: string
-    email: string
-    username: string
-    password: string
+    firstname: string;
+    lastname: string;
+    email: string;
+    username: string;
+    password: string;
 }
 
-export interface UserDataObject {
-    _id: string
-    firstname: string
-    lastname: string
-    email: string
-    username: string
-    password: string
-    createdAt: string
-    updatedAt: string
+export type UpdateUserObject = Partial<CreateUserObject>;
+
+export interface UserDataObject extends CreateUserObject {
+    _id: string;
+    createdAt?: string;
+    updatedAt?: string;
 }
 
 export interface UsersLogInDataObject {
-    username: string
-    password: string
+    username: string;
+    password: string;
 }
