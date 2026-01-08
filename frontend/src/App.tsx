a485bb1222478bfa959ac947be1890ad3e8f2be1
Girlie Razon <girlie.razon84@gmail.com>
2026-01-09 00:28:31 +0100

    refactor(app): wrap app with UserProvider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,46 +1,19 @@
 // frontend/src/App.tsx
 
-import { useEffect, useMemo, useState } from "react";
-import { UserContext } from "./utils/global/provider/UserProvider";
 import { Routing } from "./routes/Routing";
 import FooterContainer from "./components/FooterContainer";
 import NavigationBar from "./components/nav/NavigationBar";
-import { authStorage, type AuthUser } from "./utils/auth/authStorage";
+import { UserProvider } from "./utils/global/provider/UserProvider";
 
 
 function App() {
-    const [token, setToken] = useState<string | null>(null);
-    const [user, setUser] = useState<AuthUser | null>(null);
-
-    useEffect(() => {
-        setToken(authStorage.getToken());
-        setUser(authStorage.getUser());
-    }, []);
-
-    const value = useMemo(() => {
-        return {
-            token,
-            user,
-            setAuth: (nextToken: string, nextUser: AuthUser) => {
-                authStorage.setAuth(nextToken, nextUser);
-                setToken(nextToken);
-                setUser(nextUser);
-            },
-            logout: () => {
-                authStorage.clearAuth();
-                setToken(null);
-                setUser(null);
-            }
-        };
-    }, [token, user]);
-
     return (
-        <UserContext.Provider value={value}>
+        <UserProvider>
             <Routing>
                 <NavigationBar />
             </Routing>
             <FooterContainer />
-        </UserContext.Provider>
+        </UserProvider>
     );
 }
 
