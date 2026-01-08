4fdbf290f218221d3fb3374efff0e73a7db9c8c2
Girlie Razon <girlie.razon84@gmail.com>
2026-01-08 15:11:34 +0100

    style(global): simplify styled-components global style and avoid layout overrides

--- frontend/src/utils/global/global.ts
+++ frontend/src/utils/global/global.ts
@@ -1,15 +1,16 @@
-import { createGlobalStyle } from 'styled-components'
+// frontend/src/utils/global/global.ts
+
+import { createGlobalStyle } from "styled-components";
+
 
 export default createGlobalStyle`
   * {
-    margin: 0;
-    padding: 0;
-    outline: 0;
     box-sizing: border-box;
-    font-family: "Oxygen - Regular", sans-serif;
   }
 
-  #root {
-    margin: 0 auto;
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: "Oxygen - Regular", sans-serif;
   }
-`
+`;
