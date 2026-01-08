3c3667f50b40642eebc7ba83c6d32962f221cd8f
Girlie Razon <girlie.razon84@gmail.com>
2026-01-08 19:22:39 +0100

    refactor(admin): simplify AdminView layout and remove excessive breaks

--- frontend/src/view/AdminView.tsx
+++ frontend/src/view/AdminView.tsx
@@ -1,61 +1,62 @@
-import React from 'react'
-import styled from 'styled-components'
-import GetAllUsers from "../components/users/GetAllUsers";
-import DeleteUser from "../components/users/DeleteUser";
-import UpdateUser from "../components/users/UpdateUser";
+// frontend/src/view/AdminView.tsx
+
+import React from "react";
+import styled from "styled-components";
+
 import Alive from "../components/users/Alive";
+import GetAllUsers from "../components/users/GetAllUsers";
 import GetUsersById from "../components/users/GetUsersById";
+import UpdateUser from "../components/users/UpdateUser";
+import DeleteUser from "../components/users/DeleteUser";
+
 import GetAllPosts from "../components/posts/GetAllPosts";
 import GetPostById from "../components/posts/GetPostById";
 import UpdatePost from "../components/posts/UpdatePost";
 import DeletePostById from "../components/posts/DeletePostById";
 
-const AdminView = () => {
-    return (
-        <Wrapper>
-            <Alive/>
-            <br/>
-            <hr className='hr'/>
-            <GetAllUsers/>
-            <br/>
-            <hr className='hr'/>
-            <GetUsersById/>
-            <br/>
-            <hr className='hr'/>
-            <UpdateUser/>
-            <br/>
-            <hr className='hr'/>
-            <DeleteUser/>
-            <hr className='hr'/>
-            <br/>
-            <GetAllPosts/>
-            <br/>
-            <hr className='hr'/>
-            <GetPostById/>
-            <br/>
-            <hr className='hr'/>
-            <UpdatePost/>
-            <br/>
-            <hr className='hr'/>
-            <DeletePostById/>
-        </Wrapper>
-    )
-}
 
-export default AdminView
+const AdminView: React.FC = () => {
+  return (
+    <Wrapper>
+      <Alive />
+      <Hr />
+
+      <GetAllUsers />
+      <Hr />
+
+      <GetUsersById />
+      <Hr />
+
+      <UpdateUser />
+      <Hr />
+
+      <DeleteUser />
+      <Hr />
+
+      <GetAllPosts />
+      <Hr />
+
+      <GetPostById />
+      <Hr />
+
+      <UpdatePost />
+      <Hr />
+
+      <DeletePostById />
+    </Wrapper>
+  );
+};
+
+export default AdminView;
 
 const Wrapper = styled.div`
   background-color: var(--primary-color);
   display: grid;
-  grid-gap: 1em;
-  padding: 0 1em 1em 1em;
+  gap: 1.25rem;
+  padding: 3rem 1rem 1rem;
+`;
 
-  &:first-child {
-    padding-top: 3em;
-  }
-
-  .hr {
-    border: 1px dashed var(--fourthly-color);
-    width: 100%;
-  }
-`
+const Hr = styled.hr`
+  border: 1px dashed var(--fourthly-color);
+  width: 100%;
+`;
