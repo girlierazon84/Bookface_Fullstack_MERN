31c60931f0ac8e6ebccefc1897ef4caafb959a49
Girlie Razon <girlie.razon84@gmail.com>
2026-01-08 14:55:20 +0100

    fix(app): correct localStorage auth check and standardize formatting

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,28 @@
-import React, { useEffect, useState } from 'react';
-import { UserContext } from './utils/global/provider/UserProvider';
+import React, { useEffect, useState } from "react";
+import { UserContext } from "./utils/global/provider/UserProvider";
 import { Routing } from "./routes/Routing";
 import FooterContainer from "./components/FooterContainer";
 import NavigationBar from "./components/nav/NavigationBar";
 
 
 function App() {
-    const [authenticatedUser, setAuthenticatedUser] = useState<string>('')
-
-    const checkIfUserIsAuthenticatedInBrowser = () => {
-        const username = localStorage.getItem('username')
-        if (username === 'string') {
-            setAuthenticatedUser(username)
-        }
-    }
+    const [authenticatedUser, setAuthenticatedUser] = useState<string>("");
 
     useEffect(() => {
-        checkIfUserIsAuthenticatedInBrowser()
-    }, [])
+        const username = localStorage.getItem("username");
+        if (username) {
+            setAuthenticatedUser(username);
+        }
+    }, []);
 
     return (
         <UserContext.Provider value={{ authenticatedUser, setAuthenticatedUser }}>
             <Routing>
                 <NavigationBar />
             </Routing>
-            <FooterContainer/>
+            <FooterContainer />
         </UserContext.Provider>
-    )
+    );
 }
-
-
-
 
 export default App;
