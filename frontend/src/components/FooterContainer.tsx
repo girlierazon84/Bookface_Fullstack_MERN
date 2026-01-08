bd4e8747e7f316cfc984527d3098c736e1fa65b9
Girlie Razon <girlie.razon84@gmail.com>
2026-01-08 19:11:50 +0100

    refactor(footer): use semantic footer element and improve spacing

--- frontend/src/components/FooterContainer.tsx
+++ frontend/src/components/FooterContainer.tsx
@@ -1,57 +1,60 @@
-import React from 'react'
-import styled from 'styled-components'
-import Footer_Logo from '../utils/images/Footer_Logo.png'
-import CopyrightIcon from '@mui/icons-material/Copyright'
+// frontend/src/components/FooterContainer.tsx
+
+import styled from "styled-components";
+import FooterLogo from "../utils/images/Footer_Logo.png";
+import CopyrightIcon from "@mui/icons-material/Copyright";
 
 
 export default function FooterContainer() {
-    return (
-        <>
-            <Footer>
-                <InnerContainer>
-                    <img src={Footer_Logo} alt='Bookface logo'/>
-                </InnerContainer>
+  return (
+    <Footer>
+      <InnerContainer>
+        <img src={FooterLogo} alt="Bookface logo" />
+      </InnerContainer>
 
-                <h4>This website is developed by:
-                    <br/>
-                    <u>
-                        Girlie Razon
-                        <br/>
-                        Mohammad Haydar
-                    </u>
-                </h4>
-                <h5>BF <CopyrightIcon fontSize='inherit'/> 2021</h5>
-            </Footer>
-        </>
-    )
+      <h4>
+        This website is developed by:
+        <br />
+        <u>
+          Girlie Razon
+          <br />
+          Mohammad Haydar
+        </u>
+      </h4>
+
+      <h5>
+        BF <CopyrightIcon fontSize="inherit" /> 2021
+      </h5>
+    </Footer>
+  );
 }
 
-const Footer = styled.div`
+const Footer = styled.footer`
   display: grid;
-  border: 2px solid var(--fifthly-color);
-  box-shadow: 0 0 100px var(--fourthly-color);
+  border-top: 2px solid var(--fifthly-color);
+  box-shadow: 0 0 40px var(--fourthly-color);
   background-color: var(--primary-color);
+  padding: 1rem 0;
 
-  h4 {
+  h4,
+  h5 {
     color: var(--fourthly-color);
     text-align: center;
+    margin: 0.5rem 0;
   }
 
   h5 {
-    color: var(--fourthly-color);
     font-size: 1em;
-    text-align: center;
   }
-`
+`;
 
 const InnerContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  flex-direction: column;
-  
+
   img {
     max-width: 60px;
     padding-top: 10px;
   }
-`
+`;
