09a88681dde462b9a52a4db9bc75de3e0741a119
Girlie Razon <girlie.razon84@gmail.com>
2026-01-08 19:33:05 +0100

    fix(posts): make GetPostById safe with null state and JsonToTable fallback

--- frontend/src/components/posts/GetPostById.tsx
+++ frontend/src/components/posts/GetPostById.tsx
@@ -1,45 +1,52 @@
-import { useState } from 'react'
-import { JsonToTable } from 'react-json-to-table'
-import styled from 'styled-components'
-import {PostDataObject} from "../../utils/interface/PostInterface";
+// frontend/src/components/posts/GetPostById.tsx
+
+import { useState } from "react";
+import { JsonToTable } from "react-json-to-table";
+import styled from "styled-components";
+
+import type { PostDataObject } from "../../utils/interface/PostInterface";
 import PostService from "../../utils/api/service/PostService";
 
-function GetPostById() {
-    const [onePost, setOnePost] = useState<PostDataObject>()
-    const [id, setId] = useState<string>('')
 
-    function getPosts() {
-        PostService.getPostById(id)
-            .then(function (response) {
-                console.log(response.data)
-                setOnePost(response.data)
-            })
-            .catch(function (error) {
-                console.log(error)
-            })
+const GetPostById: React.FC = () => {
+  const [onePost, setOnePost] = useState<PostDataObject | null>(null);
+  const [id, setId] = useState("");
+
+  const getPost = async () => {
+    try {
+      const res = await PostService.getPostById(id);
+      setOnePost(res.data);
+    } catch {
+      setOnePost(null);
     }
+  };
 
-    function clearInputs() {
-        setId('');
-        setOnePost(undefined);
-    }
+  const clearInputs = () => {
+    setId("");
+    setOnePost(null);
+  };
 
-    return (
-        <Article>
-            <H1>Get Post by ID</H1>
-            <Input type='text'
-                   placeholder='ID'
-                   value={ id }
-                   onChange={ event => setId(event.target.value) }/>
-            <JsonToTable json={ onePost }/>
-            <br/>
-            <GridContainer>
-                <Button className='getPostById__btn' onClick={ getPosts }>Get Post By ID</Button>
-                <Button className='clear__btn' onClick={ () => clearInputs() }>Clear</Button>
-            </GridContainer>
-        </Article>
-    )
-}
+  return (
+    <Article>
+      <H1>Get Post by ID</H1>
+
+      <Input placeholder="ID" value={id} onChange={(e) => setId(e.target.value)} />
+
+      <JsonToTable json={onePost ?? {}} />
+
+      <GridContainer>
+        <Button type="button" onClick={getPost}>
+          Get Post By ID
+        </Button>
+        <Button type="button" onClick={clearInputs}>
+          Clear
+        </Button>
+      </GridContainer>
+    </Article>
+  );
+};
+
+export default GetPostById;
 
 const Article = styled.article`
   padding: 1em;
@@ -47,14 +54,13 @@ const Article = styled.article`
   box-shadow: 0 10px 8px 5px var(--fourthly-color);
   border-radius: 1em;
   background-color: var(--thirdly-color);
-  margin-top: 2em;
-`
+`;
 
 const H1 = styled.h1`
   font-size: 2em;
   color: var(--fourthly-color);
   font-family: "Oxygen - Regular", sans-serif;
-`
+`;
 
 const Input = styled.input`
   background-color: var(--fifthly-color);
@@ -63,24 +69,16 @@ const Input = styled.input`
   padding: 1em;
   border-radius: 10px;
   font-size: 1em;
-`
+  border: 1px solid var(--fifthly-color);
+`;
 
 const GridContainer = styled.div`
-  display: inline-block;
-  width: 100%;
-
-  .getPostById__btn {
-    float: left;
-
-  }
-
-  .clear__btn {
-    float: right;
-  }
-`
+  display: flex;
+  gap: 10px;
+`;
 
 const Button = styled.button`
-  width: 50%;
+  width: 100%;
   text-transform: uppercase;
   font-family: "Oxygen - Regular", sans-serif;
   font-size: 1em;
@@ -89,12 +87,11 @@ const Button = styled.button`
   border-radius: 0.8em;
   background-color: var(--secondary-color);
   color: var(--fifthly-color);
-  border-color: var(--fifthly-color);
+  border: 1px solid var(--fifthly-color);
+  cursor: pointer;
 
   &:hover {
     background-color: var(--fifthly-color);
     color: var(--secondary-color);
   }
-`
-
-export default GetPostById
+`;
