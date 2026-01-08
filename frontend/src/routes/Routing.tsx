fe0f6b8c9ae1a4c558be971523d0638387547e1b
Girlie Razon <girlie.razon84@gmail.com>
2026-01-08 23:07:43 +0100

    feat(routes): add RequireAuth guard for feed/profile/create/settings routes

--- frontend/src/routes/Routing.tsx
+++ frontend/src/routes/Routing.tsx
@@ -1,7 +1,7 @@
 // frontend/src/routes/Routing.tsx
 
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import RoutingPath from "./RoutingPath";
 
 import UsersLogInView from "../view/UsersLogInView";
@@ -13,10 +13,15 @@ import Alive from "../components/users/Alive";
 import ProfileView from "../view/ProfileView";
 import AdminView from "../view/AdminView";
 import SettingsView from "../view/SettingsView";
+import { useUserContext } from "../utils/global/provider/UserProvider";
 
 
-type RoutingProps = {
-    children?: React.ReactNode;
+type RoutingProps = { children?: React.ReactNode };
+
+const RequireAuth: React.FC<{ children: React.ReactElement }> = ({ children }) => {
+    const { token } = useUserContext();
+    if (!token) return <Navigate to={RoutingPath.usersLogInView} replace />;
+    return children;
 };
 
 export const Routing: React.FC<RoutingProps> = ({ children }) => {
@@ -24,18 +29,51 @@ export const Routing: React.FC<RoutingProps> = ({ children }) => {
         <BrowserRouter>
             {children}
             <Routes>
-                <Route path={RoutingPath.homeView} element={<HomeView />} />
+                {/* Public */}
                 <Route path={RoutingPath.usersLogInView} element={<UsersLogInView />} />
                 <Route path={RoutingPath.signUpFormView} element={<SignUpFormView />} />
-                <Route path={RoutingPath.profileView} element={<ProfileView />} />
-                <Route path={RoutingPath.createPostView} element={<CreatePostView />} />
+
+                {/* Protected */}
+                <Route
+                    path={RoutingPath.homeView}
+                    element={
+                        <RequireAuth>
+                            <HomeView />
+                        </RequireAuth>
+                    }
+                />
+                <Route
+                    path={RoutingPath.profileView}
+                    element={
+                        <RequireAuth>
+                            <ProfileView />
+                        </RequireAuth>
+                    }
+                />
+                <Route
+                    path={RoutingPath.createPostView}
+                    element={
+                        <RequireAuth>
+                            <CreatePostView />
+                        </RequireAuth>
+                    }
+                />
+                <Route
+                    path={RoutingPath.settingsView}
+                    element={
+                        <RequireAuth>
+                            <SettingsView />
+                        </RequireAuth>
+                    }
+                />
+
+                {/* Admin/misc (protect if you want) */}
                 <Route path={RoutingPath.adminView} element={<AdminView />} />
                 <Route path={RoutingPath.apiAliveView} element={<Alive />} />
-                <Route path={RoutingPath.settingsView} element={<SettingsView />} />
-                <Route path={RoutingPath.pageNotFoundView} element={<PageNotFoundView />} />
 
-                {/* Catch-all */}
-                <Route path={RoutingPath.wildCardView} element={<PageNotFoundView />} />
+                {/* 404 */}
+                <Route path={RoutingPath.pageNotFoundView} element={<PageNotFoundView />} />
+                <Route path={RoutingPath.wildCardView} element={<Navigate to={RoutingPath.pageNotFoundView} replace />} />
             </Routes>
         </BrowserRouter>
     );
