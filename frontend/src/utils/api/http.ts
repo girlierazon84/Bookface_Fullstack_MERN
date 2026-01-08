31fe8543bdb080af49750a8e7ddedfa1bd10056b
Girlie Razon <girlie.razon84@gmail.com>
2026-01-08 23:04:04 +0100

    feat(api): add axios interceptor to send Bearer token automatically

--- frontend/src/utils/api/http.ts
+++ frontend/src/utils/api/http.ts
@@ -1,6 +1,7 @@
 // frontend/src/utils/api/http.ts
 
 import axios from "axios";
+import { authStorage } from "../auth/authStorage";
 
 
 const serverUrl = process.env.REACT_APP_SERVER_URL ?? "http://localhost";
@@ -8,8 +9,15 @@ const serverPort = process.env.REACT_APP_SERVER_PORT ?? "3001";
 
 export const API_BASE_URL = `${serverUrl}:${serverPort}`;
 
-const http = axios.create({
-    baseURL: API_BASE_URL
+const http = axios.create({ baseURL: API_BASE_URL });
+
+http.interceptors.request.use((config) => {
+    const token = authStorage.getToken();
+    if (token) {
+        config.headers = config.headers ?? {};
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
 });
 
 export default http;
