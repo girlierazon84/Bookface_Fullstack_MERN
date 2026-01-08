26dbcea5f75a7c6249b77a90a88f2d3197b0316e
Girlie Razon <girlie.razon84@gmail.com>
2026-01-08 19:18:34 +0100

    refactor(view): improve ProfileView layout and accessibility

--- frontend/src/view/ProfileView.tsx
+++ frontend/src/view/ProfileView.tsx
@@ -1,57 +1,57 @@
-import React from 'react'
+// frontend/src/view/ProfileView.tsx
+
+import React from "react";
 import styled from "styled-components";
-import {useUserContext} from "../utils/global/provider/UserProvider";
+import { useUserContext } from "../utils/global/provider/UserProvider";
 
 
-const ProfileView = () => {
-    const {authenticatedUser} = useUserContext()
-    const imgUrl = 'https://thispersondoesnotexist.com/image'
+const ProfileView: React.FC = () => {
+  const { authenticatedUser } = useUserContext();
+  const imgUrl = "https://thispersondoesnotexist.com/image";
 
-    
-    return (
-        <Article>
-            <h1>My Profile</h1>
-            <GridContainer>
-                <div>
-                    <Img src={ imgUrl }/>
-                </div>
+  return (
+    <Article>
+      <h1>My Profile</h1>
 
-                <div>
-                    <SpanUserName>{ authenticatedUser }</SpanUserName>
-                </div>
-            </GridContainer>
-        </Article>
-    )
-}
+      <GridContainer>
+        <Img src={imgUrl} alt="Profile avatar" />
+        <SpanUserName>{authenticatedUser}</SpanUserName>
+      </GridContainer>
+    </Article>
+  );
+};
 
-export default ProfileView
+export default ProfileView;
 
 const Article = styled.article`
   background-color: var(--primary-color);
-  padding: 0.1em 0 18em 0;
-  
+  padding: 2rem 0 18rem 0;
+
   h1 {
     text-align: center;
     font-family: "Oleo Script", cursive;
     font-size: 3em;
     font-weight: 700;
-    color:  var(--fourthly-color);
+    color: var(--fourthly-color);
   }
-`
+`;
 
 const GridContainer = styled.div`
-  display: inline-block;
-  margin: 5%;
-`
+  display: grid;
+  place-items: center;
+  gap: 16px;
+  margin-top: 2rem;
+`;
 
 const Img = styled.img`
-  width: 30%;
+  width: min(320px, 60vw);
   border: 1px solid var(--thirdly-color);
   border-radius: 10px;
-`
+`;
 
 const SpanUserName = styled.span`
   color: var(--secondary-color);
   font-family: "Oleo Script", sans-serif;
   font-weight: bold;
-`
+  font-size: 1.5rem;
+`;
