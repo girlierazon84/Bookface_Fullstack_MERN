8b45eea4c21f2b9f2ea62d681f57b58bd14d261c
Girlie Razon <girlie.razon84@gmail.com>
2026-01-08 02:24:53 +0100

    fix(routing): import Route from react-router-dom and update Routing props typing

--- frontend/src/routes/Routing.tsx
+++ frontend/src/routes/Routing.tsx
@@ -1,36 +1,40 @@
-import React from 'react'
-import { BrowserRouter, Navigate, Routes } from "react-router-dom"
-import RoutingPath from "./RoutingPath"
-import { Route } from "react-router"
-import UsersLogInView from "../view/UsersLogInView"
-import HomeView from "../view/HomeView"
-import PageNotFoundView from "../view/PageNotFoundView"
+import React from "react";
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import RoutingPath from "./RoutingPath";
+
+import UsersLogInView from "../view/UsersLogInView";
+import HomeView from "../view/HomeView";
+import PageNotFoundView from "../view/PageNotFoundView";
 import CreatePostView from "../view/CreatePostView";
 import SignUpFormView from "../view/SignUpFormView";
-import Alive from '../components/users/Alive'
+import Alive from "../components/users/Alive";
 import ProfileView from "../view/ProfileView";
 import AdminView from "../view/AdminView";
 import SettingsView from "../view/SettingsView";
 
+type RoutingProps = {
+    children?: React.ReactNode;
+};
 
-
-export const Routing = (props: { children?: React.ReactChild }) => {
-
+export const Routing: React.FC<RoutingProps> = ({ children }) => {
     return (
         <BrowserRouter>
-            { props.children }
+            {children}
             <Routes>
-                <Route path={ RoutingPath.wildCardView } element={ <Navigate to={ RoutingPath.pageNotFoundView }/> }/>
-                <Route path={ RoutingPath.usersLogInView } element={ <UsersLogInView/> }/>
-                <Route path={ RoutingPath.signUpFormView } element={ <SignUpFormView/> }/>
-                <Route path={ RoutingPath.profileView } element={ <ProfileView/> }/>
-                <Route path={ RoutingPath.createPostView } element={ <CreatePostView/> }/>
-                <Route path={ RoutingPath.homeView } element={ <HomeView/> }/>
-                <Route path={ RoutingPath.adminView } element={ <AdminView/> }/>
-                <Route path={ RoutingPath.apiAliveView} element={ <Alive/> }/>
-                <Route path={ RoutingPath.settingsView} element={ <SettingsView/> }/>
-                <Route path={ RoutingPath.pageNotFoundView } element={ <PageNotFoundView/> }/>
+                <Route
+                    path={RoutingPath.wildCardView}
+                    element={<Navigate to={RoutingPath.pageNotFoundView} />}
+                />
+                <Route path={RoutingPath.usersLogInView} element={<UsersLogInView />} />
+                <Route path={RoutingPath.signUpFormView} element={<SignUpFormView />} />
+                <Route path={RoutingPath.profileView} element={<ProfileView />} />
+                <Route path={RoutingPath.createPostView} element={<CreatePostView />} />
+                <Route path={RoutingPath.homeView} element={<HomeView />} />
+                <Route path={RoutingPath.adminView} element={<AdminView />} />
+                <Route path={RoutingPath.apiAliveView} element={<Alive />} />
+                <Route path={RoutingPath.settingsView} element={<SettingsView />} />
+                <Route path={RoutingPath.pageNotFoundView} element={<PageNotFoundView />} />
             </Routes>
         </BrowserRouter>
-    )
-}
+    );
+};
