6ddc12323a059f7a3778984618accc60df7b86f4
Girlie Razon <girlie.razon84@gmail.com>
2026-01-08 19:07:24 +0100

    fix(nav): type Burger setOpen correctly and add accessibility attributes

--- frontend/src/components/nav/Burger.tsx
+++ frontend/src/components/nav/Burger.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import { StyledBurger } from './Burger.styled'
-import RightNav from './RightNav';
+// frontend/src/components/nav/Burger.tsx
 
-interface Props {
-    open: boolean
-    setOpen: (open: boolean) => void
-}
+import React from "react";
+import { StyledBurger } from "./Burger.styled";
+import RightNav from "./RightNav";
 
-const Burger: React.FC<Props> = ({ setOpen, open }) => {
 
+type Props = {
+    open: boolean;
+    setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const Burger: React.FC<Props> = ({ open, setOpen }) => {
     return (
         <>
-            <StyledBurger open={ open } onClick={() => setOpen(!open)}>
+            <StyledBurger
+                open={open}
+                type="button"
+                aria-label="Toggle navigation menu"
+                aria-expanded={open}
+                onClick={() => setOpen((prev) => !prev)}
+            >
                 <div />
                 <div />
                 <div />
             </StyledBurger>
-            <RightNav open={open}/>
-        </>
-    )
-}
 
-export default Burger
+            <RightNav open={open} />
+        </>
+    );
+};
+
+export default Burger;
