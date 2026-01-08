a8adc5440bb7aad090aa90bb04cc77ea5a38dd25
Girlie Razon <girlie.razon84@gmail.com>
2026-01-08 15:20:05 +0100

    chore(routing): format RoutingPath and make it immutable

--- frontend/src/routes/RoutingPath.ts
+++ frontend/src/routes/RoutingPath.ts
@@ -1,14 +1,16 @@
-const RoutingPath = {
-    wildCardView: '*',
-    usersLogInView: '/log_in',
-    signUpFormView: '/sign_up',
-    profileView: '/profile',
-    settingsView:'/settings',
-    createPostView: '/create_post',
-    homeView: '/',
-    adminView: '/admin_view',
-    apiAliveView: '/my_api_is_alive',
-    pageNotFoundView: '/404'
-}
+// frontend/src/routes/RoutingPath.ts
 
-export default RoutingPath
+const RoutingPath = {
+    wildCardView: "*",
+    usersLogInView: "/log_in",
+    signUpFormView: "/sign_up",
+    profileView: "/profile",
+    settingsView: "/settings",
+    createPostView: "/create_post",
+    homeView: "/",
+    adminView: "/admin_view",
+    apiAliveView: "/my_api_is_alive",
+    pageNotFoundView: "/404"
+} as const;
+
+export default RoutingPath;
