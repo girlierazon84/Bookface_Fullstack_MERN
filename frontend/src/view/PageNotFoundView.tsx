7d6aa22fd13f9ebc4a5292c36e0ccebc9dcc2fec
Girlie Razon <girlie.razon84@gmail.com>
2026-01-08 19:19:39 +0100

    refactor(view): replace excessive breaks with proper 404 layout

--- frontend/src/view/PageNotFoundView.tsx
+++ frontend/src/view/PageNotFoundView.tsx
@@ -1,58 +1,30 @@
-import React from 'react'
+// frontend/src/view/PageNotFoundView.tsx
+
+import React from "react";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
+import RoutingPath from "../routes/RoutingPath";
 
 
+const PageNotFoundView: React.FC = () => {
+  return (
+    <Article>
+      <h1>Page not found! (404)</h1>
+      <p>
+        Go back to <Link to={RoutingPath.homeView}>Home</Link>
+      </p>
+    </Article>
+  );
+};
 
-const PageNotFoundView = () => {
-    return (
-        <Article>
-            <h1>Page not found! 404</h1>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-        </Article>
-    )
-}
-
-export default PageNotFoundView
+export default PageNotFoundView;
 
 const Article = styled.article`
   background-color: var(--primary-color);
+  padding: 5rem 1rem 18rem;
 
-  h1 {
+  h1,
+  p {
     text-align: center;
-    margin: 10%;
   }
-`
+`;
