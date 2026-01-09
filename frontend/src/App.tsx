ba376ef27f72c82abbc741dbda8531f76a3dd703
Girlie Razon <girlie.razon84@gmail.com>
2026-01-09 19:40:03 +0100

    chore(app): wrap app with AuthBootstrap for persistent login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,15 +4,18 @@ import { Routing } from "./routes/Routing";
 import FooterContainer from "./components/FooterContainer";
 import NavigationBar from "./components/nav/NavigationBar";
 import { UserProvider } from "./utils/global/provider/UserProvider";
+import AuthBootstrap from "./utils/auth/AuthBootstrap";
 
 
 function App() {
     return (
         <UserProvider>
-            <Routing>
-                <NavigationBar />
-            </Routing>
-            <FooterContainer />
+            <AuthBootstrap>
+                <Routing>
+                    <NavigationBar />
+                </Routing>
+                <FooterContainer />
+            </AuthBootstrap>
         </UserProvider>
     );
 }
