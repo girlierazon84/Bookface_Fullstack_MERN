fa08f659332f08c1161b7efb5934b47fc20c8f98
Girlie Razon <girlie.razon84@gmail.com>
2026-01-10 02:29:12 +0100

    refactor(app): render login/signup without authenticated layout chrome

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,14 @@
 // frontend/src/App.tsx
 
 import styled from "styled-components";
+import { matchPath, useLocation } from "react-router-dom";
 
 import { Routing } from "./routes/Routing";
 import FooterContainer from "./components/FooterContainer";
 import NavigationBar from "./components/nav/NavigationBar";
 import { UserProvider } from "./utils/global/provider/UserProvider";
 import AuthBootstrap from "./utils/auth/AuthBootstrap";
-
-
-// Main application component export
-function App() {
-    return (
-        <UserProvider>
-            <AuthBootstrap>
-                <AppShell>
-                    <NavigationBar />
-                    <Main>
-                        <Routing />
-                    </Main>
-                    <FooterContainer />
-                </AppShell>
-            </AuthBootstrap>
-        </UserProvider>
-    );
-}
-
-export default App;
+import RoutingPath from "./routes/RoutingPath";
 
 
 /**-------------------------------------
@@ -39,6 +21,47 @@ const AppShell = styled.div`
     background: var(--primary-color);
 `;
 
+const PublicShell = styled.div`
+    min-height: 100vh;
+    background: var(--primary-color);
+    display: grid;
+    grid-template-rows: 1fr;
+`;
+
 const Main = styled.main`
     padding-bottom: env(safe-area-inset-bottom);
 `;
+
+// Main application component export
+function App() {
+    const location = useLocation();
+
+    // Pages that should NOT show the authenticated layout chrome (nav/footer)
+    const isPublicAuthPage =
+        matchPath({ path: RoutingPath.usersLogInView, end: true }, location.pathname) ||
+        matchPath({ path: RoutingPath.signUpFormView, end: true }, location.pathname);
+
+    return (
+        <UserProvider>
+            <AuthBootstrap>
+                {isPublicAuthPage ? (
+                    <PublicShell>
+                        <Main>
+                            <Routing />
+                        </Main>
+                    </PublicShell>
+                ) : (
+                    <AppShell>
+                        <NavigationBar />
+                        <Main>
+                            <Routing />
+                        </Main>
+                        <FooterContainer />
+                    </AppShell>
+                )}
+            </AuthBootstrap>
+        </UserProvider>
+    );
+}
+
+export default App;
