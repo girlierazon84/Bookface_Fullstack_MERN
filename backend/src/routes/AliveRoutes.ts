95e1bd371c55f3666e41e27308f524c2bf13a793
Girlie Razon <girlie.razon84@gmail.com>
2026-01-11 22:26:47 +0100

    refactor(routes): convert aliveRoutes to Router export

--- backend/src/routes/AliveRoutes.ts
+++ backend/src/routes/AliveRoutes.ts
@@ -1,13 +1,13 @@
 // backend/src/routes/aliveRoutes.ts
 
-import type { Express, Request, Response } from "express";
+import { Router } from "express";
 import statusCode from "../config/statusCode";
 
 
-const routes = (app: Express) => {
-    app.get("/", (_req: Request, res: Response) => {
-        res.status(statusCode.OK).send("My API is Alive!");
-    });
-};
+const router = Router();
 
-export default { routes };
+router.get("/", (_req, res) => {
+    res.status(statusCode.OK).send("My API is Alive!");
+});
+
+export default router;
