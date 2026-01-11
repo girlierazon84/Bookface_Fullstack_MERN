9ea9ff13e1e6d8c443eda912c1ff2cad8781ec04
Girlie Razon <girlie.razon84@gmail.com>
2026-01-11 22:54:50 +0100

    Add index module to export all modules inside middlewares for easy import

---
+++ backend/src/middlewares/index.ts
@@ -0,0 +1,8 @@
+// backend/src/middlewares/index.ts
+
+export * from "./applyMiddleware"
+export * from "./authMiddleware"
+export * from "./errorMiddleware"
+export * from "./morganMiddleware"
+export * from "./uploadMiddleware"
+export * from "./validateSchema"
