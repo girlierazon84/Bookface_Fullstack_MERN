80b83917eefa1afe5de69417d56c668a647b8c7b
Girlie Razon <girlie.razon84@gmail.com>
2026-01-11 22:48:14 +0100

    Add index module to export modules inside schemas module to easy import the modules

---
+++ backend/src/schemas/index.ts
@@ -0,0 +1,9 @@
+// backend/src/schemas/index.ts
+
+export * from "./auth.schema"
+export * from "./comment.schema"
+export * from "./common.schema"
+export * from "./friend.schema"
+export * from "./media.schema"
+export * from "./post.schema"
+export * from "./user.schema"
