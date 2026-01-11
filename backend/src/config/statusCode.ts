3d3f9c02d85d4556652e889f1f39c59a419f3eeb
Girlie Razon <girlie.razon84@gmail.com>
2026-01-11 20:28:10 +0100

    chore(http): centralize status codes

---
+++ backend/src/config/statusCode.ts
@@ -0,0 +1,16 @@
+// backend/src/config/statusCode.ts
+
+const statusCode = {
+    OK: 200,
+    CREATED: 201,
+
+    BAD_REQUEST: 400,
+    UNAUTHORIZED: 401,
+    FORBIDDEN: 403,
+    NOT_FOUND: 404,
+    METHOD_NOT_ALLOWED: 405,
+
+    INTERNAL_SERVER_ERROR: 500
+} as const;
+
+export default statusCode;
