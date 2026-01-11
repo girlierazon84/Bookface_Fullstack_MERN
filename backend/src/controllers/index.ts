eb20620376d6923d0268d40d1cc685db9774221e
Girlie Razon <girlie.razon84@gmail.com>
2026-01-11 22:58:39 +0100

    Add index module to export all modules inside controllers for easy import

---
+++ backend/src/controllers/index.ts
@@ -0,0 +1,9 @@
+// backend/src/controllers/index.ts
+
+export * from "./authController"
+export * from "./commentController"
+export * from "./feedController"
+export * from "./friendController"
+export * from "./postController"
+export * from "./userController"
+export * from "./userMediaController"
